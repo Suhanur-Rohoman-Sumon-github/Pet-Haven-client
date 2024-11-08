7797598e2281c98b2a977c3808246be916a67d7f
Suhnaur-Rohoman-Sumon-github <sumonsuhanurrohoman@gmail.com>
2024-11-09 04:39:18 +0600

    product page added

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <div>
+        <h1 className="text-red-500 underline ">this is my app bro</h1>
+        <Button>click me</Button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
