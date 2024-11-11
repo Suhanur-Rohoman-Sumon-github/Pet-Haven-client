97078beab1fc307b344f5316fb89025c09f3b73b
Md Foisal Islam <islammdfoisal54@gmail.com>
2024-11-11 10:56:36 +0600

    top nav section done

---
+++ src/pages/sharedPages/Navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import TopNav from '../../../components/Navbar/TopNav';
+
+const Navbar = () => {
+  return (
+    <nav className="bg-primary container">
+      <TopNav />
+    </nav>
+  );
+};
+
+export default Navbar;
